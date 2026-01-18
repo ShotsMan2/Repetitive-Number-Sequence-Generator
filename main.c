54a4150a454bd42753582a3d6ff4a293da5f83b8
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:07:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,25 +3,27 @@
 
 int main()
 {
-    int sayi,i,max1,max2;
+    int sayi, i, max1, max2;
 
-    max1=-99999;
-    max2=-99999;
+    max1 = -99999;
+    max2 = -99999;
 
-    for(i=1;i<=10;i++){
-        printf("%d. sayiyi girin",i);
-        scanf("%d",&sayi);
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d. sayiyi girin", i);
+        scanf("%d", &sayi);
 
-        if(sayi>max1){
-            max2=max1;
-            max1=sayi;
+        if (sayi > max1)
+        {
+            max2 = max1;
+            max1 = sayi;
         }
-        else if(max2<sayi && max1!=sayi){
-            max2=sayi;
+        else if (max2 < sayi && max1 != sayi)
+        {
+            max2 = sayi;
         }
     }
-    printf("max2: %d",max2);
-
+    printf("max2: %d", max2);
 
     return 0;
 }
